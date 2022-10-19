c4bb5afb887c005de54c818a27b526c8f921bfa1
MasBr00 <creepyzodiak@gmail.com>
2022-10-19 14:30:33 +0700

    tes

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,6 +4,12 @@ module.exports = {
     PASSWORD: "1234",
     DB: "db_plexus",
     dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+          require: true, // This will help you. But you will see nwe error
+          rejectUnauthorized: false // This line will fix new error
+        }
+    },
     pool: {
         max: 5,
         min: 0,
