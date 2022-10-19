8fa13dfdce091b5e0c2ca3b4792b73ab4abd65b2
MasBr00 <creepyzodiak@gmail.com>
2022-10-19 13:53:48 +0700

    DB

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "1234",
-    DB: "db_plexus",
+    HOST: "ec2-54-80-123-146.compute-1.amazonaws.com",
+    USER: "qzngpkbjncvrai",
+    PASSWORD: "5d2831c14a55b479b86732938d32d0ad0ee3eaa4f8db6a3c3b894acb7765e1ee",
+    DB: "deitg2b1o2l1cd",
     dialect: "postgres",
     pool: {
         max: 5,
