94c88fcbb559d197f7a0124dcc96208d718e5cf9
MasBr00 <creepyzodiak@gmail.com>
2022-10-19 13:30:59 +0700

    db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,9 @@
 module.exports = {
-    HOST: "ec2-44-210-36-247.compute-1.amazonaws.com",
-    USER: "cpbglqzzlnfyaz",
-    PASSWORD: "b12545187f33523e41fae81220e6da9c9124aff8dc889a3a7e6d2bc3767638e1",
-    DB: "djm7q46l9n4t6",
+    HOST: "localhost",
+    USER: "postgres",
+    PASSWORD: "1234",
+    DB: "db_plexus",
     dialect: "postgres",
-    ssl: true,
-    dialectOptions:{
-        ssl: true
-    },
     pool: {
         max: 5,
         min: 0,
