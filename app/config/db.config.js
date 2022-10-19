c25a7b99deecf8a6af5a3126f4109e2b8918fd10
MasBr00 <creepyzodiak@gmail.com>
2022-10-19 16:59:06 +0700

    ganti db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    URL: "postgres://btrltsctsgfmtm:f8333b8e6dca716ee769c0f08b516adacb713d1a352b57298324cd0719d5e610@ec2-54-173-237-110.compute-1.amazonaws.com:5432/d2ca7i104ug1ha",
-    HOST: "ec2-54-173-237-110.compute-1.amazonaws.com",
-    USER: "btrltsctsgfmtm",
-    PASSWORD: "f8333b8e6dca716ee769c0f08b516adacb713d1a352b57298324cd0719d5e610",
-    PORT: 5432,
-    DB: "d2ca7i104ug1ha",
+    // URL: "postgres://btrltsctsgfmtm:f8333b8e6dca716ee769c0f08b516adacb713d1a352b57298324cd0719d5e610@ec2-54-173-237-110.compute-1.amazonaws.com:5432/d2ca7i104ug1ha",
+    HOST: "postgresql-92221-0.cloudclusters.net",
+    USER: "user",
+    PASSWORD: "12345678",
+    PORT: 17591,
+    DB: "db_plexus",
     dialect: "postgres",
     dialectOptions: {
         ssl: {
