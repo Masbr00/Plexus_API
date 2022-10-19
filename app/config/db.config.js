5ca23d794fa023ec67709b3cf33cf3810d8b15f0
MasBr00 <creepyzodiak@gmail.com>
2022-10-19 15:56:47 +0700

    test db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    URL: "postgres://frlnbomh:as2o90J7I8r7PFTJvI8UhQwdpsdQZKWA@tiny.db.elephantsql.com/frlnbomh",
-    HOST: "tiny.db.elephantsql.com",
-    USER: "frlnbomh",
-    PASSWORD: "as2o90J7I8r7PFTJvI8UhQwdpsdQZKWA",
+    URL: "postgres://qkbsgvbwhmgqoh:3498152f3abda72c4d7f8d495d129790f1775a78e157067408aaedec204c09b4@ec2-3-223-242-224.compute-1.amazonaws.com:5432/d8sg2gr9eit0le",
+    HOST: "ec2-3-223-242-224.compute-1.amazonaws.com",
+    USER: "qkbsgvbwhmgqoh",
+    PASSWORD: "3498152f3abda72c4d7f8d495d129790f1775a78e157067408aaedec204c09b4",
     PORT: 5432,
-    DB: "frlnbomh",
+    DB: "d8sg2gr9eit0le",
     dialect: "postgres",
     dialectOptions: {
         ssl: {
