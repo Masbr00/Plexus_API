6d5e77748e1c4e395c2c37de242150b1f12ca24a
MasBr00 <creepyzodiak@gmail.com>
2022-10-24 00:40:03 +0700

    tes cyclic

--- app/controller/userManager.controller.js
+++ app/controller/userManager.controller.js
@@ -73,7 +73,7 @@ exports.updateProfil = async (req, res) => {
         const fileName = `${date}-${time}-${newName}.jpg`;
         // const filePath = __dirname + "../../../public/image/" + `${fileName}`; //lokasi ketika run di localhost
         // const filePath = __dirname + "/public/image/" + `${fileName}`; //lokasi ketika run di cyclic
-        const filePath = __dirname + "/var/task/app/controller/public/image/" + `${fileName}`; //lokasi ketika run di cyclic
+        const filePath = __dirname + "/public/image/" + `${fileName}`; //lokasi ketika run di cyclic
         const allowedExtension = ['image/png','image/jpg','image/jpeg'];
 
         const data = await UpdateProfil.findOne({
