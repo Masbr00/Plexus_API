f46bb4206662ddecc5603c81cd0903c8ec4cc06c
MasBr00 <creepyzodiak@gmail.com>
2022-10-24 15:23:48 +0700

    adding forgotten path

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser")
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require("path");
 
 global.__basedir = __dirname;
 
